88078f332134baeb510900697ba380a4d386066e
Appsxxl <appsxxl@github.com>
2026-04-28 14:34:45 +0000

    Fix: Configure Metro to exclude .cache directories to fix build issues

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,24 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
+const path = require("path");
 
 const config = getDefaultConfig(__dirname);
+
+// Exclude .cache directories from Metro's watch list
+config.projectRoot = __dirname;
+
+// Add blocklist for cache directories
+const baseBlockList = config.resolver?.blockList || [];
+const cacheBlockList = [
+  /node_modules\/.*\.cache\/.*/,
+  /\.cache\/.*/,
+  /\.metro-health-check\/.*/,
+];
+
+config.resolver = {
+  ...config.resolver,
+  blockList: [...baseBlockList, ...cacheBlockList],
+};
 
 module.exports = withNativeWind(config, {
   input: "./global.css",
